46b07d2fe2c3ab1ff54a6a62f6cc051bd18d6922
Jonas Gesch <jonas.gesch@gmail.com>
2025-04-11 12:54:45 +0800

    Add cell class

---
+++ graphics.py
@@ -0,0 +1,69 @@
+from tkinter import Tk, BOTH, Canvas
+
+
+class Point():
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+
+
+class Line():
+    def __init__(self, start_point: Point, end_point):
+        self.p1 = start_point
+        self.p2 = end_point
+
+    def draw(self, canvas: Canvas, color: str) -> None:
+        canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=color, width=2)
+
+
+class Cell():
+    def __init__(self, window: "Window", top_left: Point, bottom_right: Point):
+        self.__win = window
+        self.__p1 = top_left
+        self.__p2 = bottom_right
+        self.has_left_wall = True
+        self.has_top_wall = True
+        self.has_right_wall = True
+        self.has_bottom_wall = True
+
+    def set_walls(self, left: bool, top: bool, right: bool, bottom: bool) -> None:
+        self.has_left_wall = left
+        self.has_top_wall = top
+        self.has_right_wall = right
+        self.has_bottom_wall = bottom
+
+    def draw(self, color) -> None:
+        # Draw walls based on top-left and bottom-right points
+        if self.has_left_wall:
+            self.__win.draw_line(Line(self.__p1, Point(self.__p1.x, self.__p2.y)), color)
+        if self.has_top_wall:
+            self.__win.draw_line(Line(self.__p1, Point(self.__p2.x, self.__p1.y)), color)
+        if self.has_right_wall:
+            self.__win.draw_line(Line(self.__p2, Point(self.__p2.x, self.__p1.y)), color)
+        if self.has_bottom_wall:
+            self.__win.draw_line(Line(self.__p2, Point(self.__p1.x, self.__p2.y)), color)
+
+
+class Window():
+    def __init__(self, width: int, height: int):
+        self.__root = Tk()
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        self.__root.wm_title("Maze Solver")
+        self.__canvas = Canvas(self.__root, background="white", height=height, width=width)
+        self.__canvas.pack(fill=BOTH, expand=1)
+        self.__isRunnning = False
+
+    def redraw(self) -> None:
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self) -> None:
+        self.__isRunnning = True
+        while self.__isRunnning == True:
+            self.redraw()
+
+    def close(self) -> None:
+        self.__isRunnning = False
+
+    def draw_line(self, line: Line, color: str) -> None:
+        line.draw(self.__canvas, color)
--- main.py
+++ main.py
@@ -1,9 +1,19 @@
+#!/usr/bin/env python3
 import sys
-from window import Window
+from graphics import Window, Point, Line, Cell
 
 
 def main() -> int:
     win = Window(800, 600)
+    line = Line(Point(69, 32), Point(420, 123))
+    cell1 = Cell(win, Point(10, 10), Point(20, 20))
+    cell1.draw("gray")
+    cell1.set_walls(False, False, False, True)
+    cell1.draw("red")
+    cell2 = Cell(win, Point(50, 50), Point(100, 150))
+    cell2.set_walls(True, False, True, False)
+    cell2.draw("blue")
+    win.draw_line(line, "red")
     win.wait_for_close()
 
     return 0
